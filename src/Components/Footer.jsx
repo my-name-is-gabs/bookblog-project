6e1384b3b02502c2bb8a1c6147ec2b839734c78f
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2022-11-28 17:12:27 +0800

    Finished front-end

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="mini-about">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+            mollitia illo. Quasi praesentium asperiores deleniti eaque
+            voluptatibus ex repellat nisi eligendi delectus expedita obcaecati,
+            explicabo sequi quia rerum dicta sunt!
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi iusto
+            dolore ipsa quidem, dolorum modi neque sed ullam laborum iure?
+            Eveniet dolorem officiis est quae quis delectus iusto
+          </p>
+        </div>
+        <div className="subscribe-container">
+          <h1>Subscribe To Get The Latest Posts</h1>
+
+          <div className="subscribe-control">
+            <button className="btn-join">Join</button>
+            <input
+              type="email"
+              className="subscribe-email"
+              name="email"
+              placeholder="Enter Email..."
+            />
+          </div>
+        </div>
+        <div className="media-links">
+          <h1>Follow Us</h1>
+          <div className="media-link">
+            <a href="#facebook">
+              <i class="bx bxl-facebook-circle"></i>
+            </a>
+            <a href="#twitter">
+              <i class="bx bxl-twitter"></i>
+            </a>
+            <a href="#instagram">
+              <i class="bx bxl-instagram"></i>
+            </a>
+            <a href="#reddit">
+              <i class="bx bxl-reddit"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="copyright-footer">
+        <p>Copyright © 2022. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
